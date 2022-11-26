d6c70d366ad2e6ce541a44bac3fd794c110c358d
Nepsis <93186075+Nepsis7@users.noreply.github.com>
2022-11-26 09:40:26 +0100

    initial commit

---
+++ BinariesKiller/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinariesKiller
+{
+    internal class Program
+    {
+        static void DeleteFolder(string _name)
+        {
+            if (Directory.Exists(_name))
+            {
+                Console.WriteLine($"deleted {_name}/");
+                Directory.Delete(_name, true);
+            }
+        }
+        static void DeleteFilesOfExtension(string _ext)
+        {
+            FileInfo[] _files = new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles($"*.{_ext}");
+            for (int i = 0; i < _files.Length; i++)
+            {
+                Console.WriteLine($"deleted {_files[i].Name}");
+                if (File.Exists(_files[i].Name))
+                    File.Delete(_files[i].Name);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string[] _folders = { "Binaries", ".vs", "Intermediate", "Sript", "DerivedDataCache" };
+            foreach (string _folder in _folders)
+                DeleteFolder(_folder);
+            DeleteFilesOfExtension("sln");
+        }
+    }
+}
